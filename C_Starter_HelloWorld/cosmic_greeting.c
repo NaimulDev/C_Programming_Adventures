d457a1910278840482c517558297b287c61fa21b
Nimul99 <htnimul99@gmail.com>
2025-02-05 21:44:48 +0600

    🚀 Initialize C Programming Adventure

---
+++ C_Starter_HelloWorld/cosmic_greeting.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("🚀 Greetings from Planet C!\n");
+    printf("   ___    _  _  ____\n");
+    printf("  / __)  ( \\/ )(_  _)\n");
+    printf(" ( (__    \\  /  _)(_\n");
+    printf("  \\___)   (__) (____)\n");
+    return 0;
+}
